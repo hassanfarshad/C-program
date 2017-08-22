28299545532cea1facfeb709f2f75ff0a8870827
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2017-08-22 15:05:12 +0530

    Create p.c

---
+++ p.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the number :");
+scanf("%d",&n);
+if(n > 0)
+{
+printf("the given number is positive");
+else
+printf("the number is negative ");
+}
+getch();
+}
