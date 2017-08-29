65c18777ce4f6b7149ba2f566429aee122099277
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2017-08-29 14:50:10 +0530

    Create oo.c

---
+++ oo.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+vooooid main()
+{
+int n;
+printf("Enter the number\n");
+scanf("%d");
+if(n%2==0)
+{
+printf("the given number is EVEN\n");
+}
+else
+{
+printf("the given number is OOD\n");
+}
+getch();
+}
