7337ee9b7f3a746744a9b7d0be2985c4953b07e2
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-02-12 16:48:07 +0530

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    printf("Enter the element1:");
+    scanf("%d%d",&a,&b);
+    printf("\nEnter the element2:");
+    scanf("%d%d",&c,&d);
+    printf("\nHOUR is:%d.%d",a-c,b-d);
+}
