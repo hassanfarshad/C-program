f8057a356ed18c95787abf34c212e8a0325a7c40
hassanfarshad <31240277+hassanfarshad@users.noreply.github.com>
2018-02-12 15:52:18 +0530

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char a[10],i;
+	printf("Enter the string");
+	scanf("%s",&a[i]);
+	if('0'<=a[i]&&a[i]<='1000')
+	{
+		printf("\nYES");
+	}
+	else
+	{
+		printf("\nNO");
+	}
+}
